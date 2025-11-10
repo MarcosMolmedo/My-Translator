7c6c52e199b836b2ea77d3963346d9f0679422fe
¨Marcos <marcosmolmedo@gmail.com>
2025-11-10 22:18:53 +0100

    Se le cambian algunos estilos al header, segun recomendacion de la disenadora

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 import "../Styles/Header.css";
-import logo from "../assets/images/perfect_circular_logo.png"; 
+import logo from "../assets/images/perfect_circular_logo.png";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setIsMenuOpen((prev) => !prev);
   };
 
   const closeMenu = () => {
@@ -16,32 +16,102 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="container">
-        <Link to="/" className="logo-title" onClick={closeMenu}>
-          <img src={logo} alt="My Translator Logo" className="logo" />
-          <h1>MY Translator</h1>
+      <div className="header__container">
+        <Link
+          to="/"
+          className="header__brand"
+          onClick={closeMenu}
+        >
+          <img
+            src={logo}
+            alt="My Translator Logo"
+            className="header__logo"
+          />
+          <h1 className="header__title">MY Translator</h1>
         </Link>
-        <div 
-          className="hamburger" 
-          onClick={toggleMenu} 
-          role="button" 
-          tabIndex="0"
-          aria-label="Menú de navegación" 
+
+        {/* Botón hamburguesa (solo mobile) */}
+        <button
+          className="header__hamburger"
+          onClick={toggleMenu}
+          type="button"
+          aria-label="Menú de navegación"
           aria-expanded={isMenuOpen}
         >
           &#9776;
-        </div>
-        
-        <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
-          {/* Botón de cierre "X" */}
-          <div className="close-menu" onClick={closeMenu}>&times;</div>
-          <ul>
-            <li><Link to="/" onClick={closeMenu}>Inicio</Link></li>
-            <li><Link to="/sobre-mi" onClick={closeMenu}>Sobre mí</Link></li>
-            <li><Link to="/servicios" onClick={closeMenu}>Servicios</Link></li>
-            <li><Link to="/cotizaciones" onClick={closeMenu}>Cotizaciones</Link></li>
-            <li><Link to="/preguntasfrecuentes" onClick={closeMenu}>Preguntas Frecuentes</Link></li>
-            <li><Link to="/opiniones" onClick={closeMenu}>Opiniones</Link></li>
+        </button>
+
+        {/* NAV */}
+        <nav
+          className={`header__nav ${
+            isMenuOpen ? "header__nav--open" : ""
+          }`}
+        >
+          {/* Botón cierre (solo mobile) */}
+          <button
+            className="header__close"
+            type="button"
+            onClick={closeMenu}
+            aria-label="Cerrar menú"
+          >
+            &times;
+          </button>
+
+          <ul className="header__nav-list">
+            <li className="header__nav-item">
+              <Link
+                to="/"
+                className="header__nav-link"
+                onClick={closeMenu}
+              >
+                Inicio
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link
+                to="/sobre-mi"
+                className="header__nav-link"
+                onClick={closeMenu}
+              >
+                Sobre mí
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link
+                to="/servicios"
+                className="header__nav-link"
+                onClick={closeMenu}
+              >
+                Servicios
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link
+                to="/cotizaciones"
+                className="header__nav-link"
+                onClick={closeMenu}
+              >
+                Cotizaciones
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link
+                to="/preguntasfrecuentes"
+                className="header__nav-link"
+                onClick={closeMenu}
+              >
+                Preguntas Frecuentes
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link
+                to="/opiniones"
+                className="header__nav-link"
+                onClick={closeMenu}
+              >
+                Opiniones
+              </Link>
+            </li>
           </ul>
         </nav>
       </div>
